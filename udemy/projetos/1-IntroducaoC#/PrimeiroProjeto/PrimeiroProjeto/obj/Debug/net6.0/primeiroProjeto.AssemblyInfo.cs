8acbc00d1a33e1584b7b7ebcf95c705bb2ff6ae3
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-07-01 23:52:54 -0300

    retomando estudos do curos de c#. adicionei dois exercícios após realizar a recapitulação

--- udemy/projetos/1-IntroducaoC#/PrimeiroProjeto/PrimeiroProjeto/obj/Debug/net6.0/primeiroProjeto.AssemblyInfo.cs
+++ udemy/projetos/1-IntroducaoC#/PrimeiroProjeto/PrimeiroProjeto/obj/Debug/net6.0/primeiroProjeto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65208aa4f085c0c4881d74458fcc5ec0427a037f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ udemy/projetos/2-RecapitulandoLogicaProgramacao/ExerciciosPropostosPt2/Exercise1Pt2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise1Pt2 {
+    internal class Program {
+        static void Main(string[] args) {
+            // faça um programa para ler um número inteiro e dizer se ele é negativo ou não
+
+            int num; 
+            bool isnegative;
+
+            Console.Write("Digite um número: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num < 0) {
+                Console.WriteLine("NEGATIVO");
+            }
+            else {
+                Console.WriteLine("NÃO NEGATIVO");
+            }
+
+           
+
+
+        }
+    }
+}
