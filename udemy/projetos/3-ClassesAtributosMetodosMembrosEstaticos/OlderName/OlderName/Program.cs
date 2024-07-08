1993ac1d6d58ad3203c77b2b09a3f624d24e7d57
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-07-07 22:24:41 -0300

    Exercício de fixação. Quem é mais velho? nome da pessoa. Resolvi usando POO.

---
+++ udemy/projetos/3-ClassesAtributosMetodosMembrosEstaticos/OlderName/OlderName/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+namespace OlderName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1, person2;
+
+            person1 = new Person();
+            person2 = new Person();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            person1.Name = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            person1.Age = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Dados da Segunda pessoa: ");
+            Console.Write("Nome: ");
+            person2.Name = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            person2.Age = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            if (person1.Age > person2.Age)
+            {
+                Console.WriteLine($"A pessoa mais velha é: {person1.Name}");
+            }
+            else if (person2.Age > person1.Age) { Console.WriteLine($"A pessoa mais velha é: {person2.Name}"); }
+
+            else { Console.WriteLine("Ninguém é mais velho"); }
+
+        }
+
+
+    }
+}
