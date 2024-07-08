aaa2be818d76158ee7d9a352c1547cffae0e4f22
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-07-08 18:51:45 -0300

    Exercício de revisão. Fácil. Simplesmente para lembrar como criar classe. Nomear classe na função Main. Instanciar uma classe e usar seus atributos, tornando assim seu programa mais coeso.

---
+++ udemy/projetos/3-ClassesAtributosMetodosMembrosEstaticos/MeanSalary/MeanSalary/Funcionario.cs
@@ -0,0 +1,9 @@
+﻿namespace MeanSalary
+{
+    internal class Funcionario
+    {
+        public double Salary;
+        public string Name;
+
+    }
+}
---
+++ udemy/projetos/3-ClassesAtributosMetodosMembrosEstaticos/MeanSalary/MeanSalary/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace MeanSalary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //faça um programa para ler: nome e salário ( 2 )
+            // mostre o salário médio dos funcionários
+            Funcionario Func1, Func2;
+            
+            Func1 = new Funcionario();
+            Func2 = new Funcionario();
+
+            //funcionário 1
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            Func1.Name = Console.ReadLine();
+
+            Console.Write("Salário: ");
+            Func1.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Funcionário 2
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            Func1.Name = Console.ReadLine();
+
+            Console.Write("Salário: ");
+            Func2.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // calcula a média dos salários
+            double Media = Mean(Func1.Salary, Func2.Salary);
+
+            Console.WriteLine($"Salário médio: {Media.ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+        }
+
+        static double Mean(double x, double y)
+        {
+            double MeanXY = (x + y) / 2.0;
+
+            return MeanXY;
+        }
+
+
+    }
+}
