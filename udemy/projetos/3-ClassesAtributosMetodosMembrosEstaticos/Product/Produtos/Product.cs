33585a2212103a2d61b6cc5b933454e8a5b1cb32
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-07-08 20:35:07 -0300

    Implementação de um produto que está definido por regras da linguagem UML. Este produto têm de atributos: nome, preço e quantidade. É possível adicionar produtos, Remover produtos e calcular o valor total no estoque.!

---
+++ udemy/projetos/3-ClassesAtributosMetodosMembrosEstaticos/Product/Produtos/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produtos
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Amount;
+        public double TotalValueInStock()
+        {
+            double TotalValue;
+            TotalValue = Price * Amount;
+
+            return TotalValue;
+        }
+
+        public void AddQuantityProducts(int Quantity)
+        {
+            Amount += Quantity;
+
+        }
+
+        public void RemoveQuantityProducts(int Quantity)
+        {
+            Amount -= Quantity;
+        }
+
+
+    }
+}
---
+++ udemy/projetos/3-ClassesAtributosMetodosMembrosEstaticos/Product/Produtos/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Globalization;
+
+namespace Produtos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando meu produto
+            Product product = new Product();
+
+            //leitura inicial de dados
+            Console.WriteLine("Entre os Dados do Produto: ");
+            Console.Write("Nome: ");
+            product.Name = Console.ReadLine();
+
+            Console.Write("Preço:");
+            product.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade Total no Estoque: ");
+            product.Amount = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Mostrando os dados coletados
+            double TotalValueProduct = product.TotalValueInStock();
+            
+            Console.WriteLine();
+            Console.Write("Dados do produto: ");
+            Console.Write($"{product.Name}, ${product.Price.ToString("F2", CultureInfo.InvariantCulture)}, ");
+            Console.Write($"{product.Amount} unidades, ");
+            Console.WriteLine($"Total: $ {TotalValueProduct.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine();
+
+            // adicionando produtos ao estoque
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int AddQuantity = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            product.AddQuantityProducts(AddQuantity);
+            TotalValueProduct = product.TotalValueInStock();
+
+            //Mostrando dados atualizados
+            Console.WriteLine();
+            Console.Write("Dados atualizados: ");
+            Console.Write($"{product.Name}, ${product.Price.ToString("F2", CultureInfo.InvariantCulture)}, ");
+            Console.Write($"{product.Amount} unidades, ");
+            Console.WriteLine($"Total: $ {TotalValueProduct.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine();
+
+            // removendo produtos do estoque
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            int RemoveQuantity = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            product.RemoveQuantityProducts(RemoveQuantity);
+            TotalValueProduct = product.TotalValueInStock();
+
+            //Mostrando dados atualizados
+            Console.WriteLine();
+            Console.Write("Dados atualizados: ");
+            Console.Write($"{product.Name}, ${product.Price.ToString("F2", CultureInfo.InvariantCulture)}, ");
+            Console.Write($"{product.Amount} unidades, ");
+            Console.WriteLine($"Total: $ {TotalValueProduct.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine();
+
+        }
+    }
+}
