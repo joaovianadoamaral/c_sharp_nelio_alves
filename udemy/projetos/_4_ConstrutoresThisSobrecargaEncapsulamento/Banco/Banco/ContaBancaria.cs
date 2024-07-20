c5a50cca2898320cb8c2a933614393f8ae07f7f8
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-07-20 13:20:10 -0300

    Exercicio de fixacao para as aulas : construtores, properties, autoproperties, get, set, encapsulamento, palavra this. Criacao de uma classe ContaBancaria que realiza operacoes de Saque e Deposito, armazena o NomeTitular e NumeroConta.

---
+++ udemy/projetos/_4_ConstrutoresThisSobrecargaEncapsulamento/Banco/Banco/ContaBancaria.cs
@@ -0,0 +1,129 @@
+﻿using System.Globalization;
+
+namespace Banco
+{
+    internal class ContaBancaria
+    {
+        private string _nomeTitularConta;
+        public decimal Saldo { get; private set; }
+        public ulong NumeroConta { get; private set; }
+
+        public ContaBancaria()
+        {
+            Console.Write("Entre o número da conta: ");
+            NumeroConta =  ulong.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            NomeTitularConta = Console.ReadLine();
+            
+            DepositoInicial();
+        }
+
+        public string NomeTitularConta
+        {
+            get
+            {
+                return _nomeTitularConta;
+            }
+            set
+            {
+                string? Nome;
+                int TamanhoString = value.Length;
+                bool NomeNulo = value == null;
+                bool NomeMaiorQueUmaLetra = TamanhoString > 1;
+
+                _nomeTitularConta = value;
+
+                if (NomeNulo || !NomeMaiorQueUmaLetra)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Digite um campo não nulo para o nome do titular");
+
+                        Nome = Console.ReadLine();
+                        if (Nome != null)
+                        {
+                            _nomeTitularConta = Nome;
+                            break;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        public void Deposito(decimal deposito)
+        {
+            bool DepositoValido = deposito > 0;
+            if (DepositoValido)
+            {
+                Saldo += deposito;
+            }
+        }
+
+        public void Saque(decimal saque)
+        {
+            int Taxa = 5;
+            
+            bool SaqueSuperiorAoSaldo= (saque + Taxa) > Saldo;
+            bool SaqueNulo = saque <= 0;
+
+            if (SaqueSuperiorAoSaldo || SaqueNulo)
+            {
+                Console.WriteLine("Você não pode sacar esse valor!");
+            }
+
+            if (!SaqueSuperiorAoSaldo && !SaqueNulo)
+            {
+                Saldo -= (saque + Taxa);
+            }
+        }
+
+        private void DepositoInicial()
+        {
+            string? RespostaUsuario;
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            RespostaUsuario = Console.ReadLine().ToLower();
+
+            bool Depositar = RespostaUsuario == "s";
+            bool NaoDepositar = RespostaUsuario == "n";
+
+            bool RespostaInvalida = RespostaUsuario != "s" && RespostaUsuario != "n";
+
+            if (RespostaInvalida)
+            {
+                DepositoInicial();
+            }
+
+            if (Depositar){
+                decimal ValorInicial;
+                Console.Write("Entre com o valor do depósito inicial: ");
+                ValorInicial = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Deposito(ValorInicial);
+
+            }
+
+            if (NaoDepositar)
+            {
+                decimal ValorInicial = 0;
+                Deposito(ValorInicial);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string message;
+
+            message =
+                $"Conta {NumeroConta}, " +
+                $"Titular: {_nomeTitularConta}, " +
+                $"Saldo: ${Saldo.ToString("F2", CultureInfo.InvariantCulture)}" +
+                "\n";
+
+            return message;
+        }
+
+    }
+}
---
+++ udemy/projetos/_4_ConstrutoresThisSobrecargaEncapsulamento/Banco/Banco/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal UltimoSaque, UltimoDeposito;
+
+            ContaBancaria joao = new ContaBancaria();
+
+            Console.WriteLine();
+            MostrarDadosConta(joao);
+
+            Console.Write("Entre um valor para depósito: ");
+            UltimoDeposito = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            joao.Deposito(UltimoDeposito);
+
+            MostrarDadosConta(joao);
+
+            Console.Write("Entre um valor para saque: ");
+            UltimoSaque = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            joao.Saque(UltimoSaque);
+            
+            MostrarDadosConta(joao);
+
+        }
+
+        static void MostrarDadosConta(object conta)
+        {
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+
+    }
+}
