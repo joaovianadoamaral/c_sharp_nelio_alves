553f739f7878787decfd4a409951d7bacde26027
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-07-29 20:55:41 -0300

    trabalhando com vetores que armazenam classes. No caso a classe trabalhada foi produto.

---
+++ udemy/projetos/_6_vetores/Products/MeanPrice/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace MeanPrice
+{
+    internal class Product
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        
+
+    }
+}
---
+++ udemy/projetos/_6_vetores/Products/MeanPrice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+namespace MeanPrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // n produtos
+            // cada produto possui preço e nome
+
+            Console.WriteLine("Digite a quantidade de produtos: ");
+            int AmountProducts = int.Parse(Console.ReadLine());
+
+            Product[] products = new Product[AmountProducts];
+
+            for (int cont = 0; cont < AmountProducts; cont++)
+            {
+                products[cont] = new Product();
+
+                Console.WriteLine($"Insira o nome do produto {cont + 1}: ");
+                products[cont].Name = Console.ReadLine();
+
+                Console.WriteLine($"Insira o preço do produto {cont + 1}: ");
+                products[cont].Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            }
+
+            double Sum = 0;
+            foreach (Product prod in products)
+            {
+                Sum += prod.Price;
+            }
+
+            double Mean = Sum / AmountProducts;
+            Console.WriteLine($"AVERAGE PRICE = {Mean.ToString("F2", CultureInfo.InvariantCulture)}");
+            
+        }
+    }
+}
