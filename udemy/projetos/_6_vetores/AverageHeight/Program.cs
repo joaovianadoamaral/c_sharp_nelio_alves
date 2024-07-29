f8b3a40ee167dfee51f5f010fade3341a77fcfaf
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-07-29 20:26:01 -0300

    trabalhando com vetores dinâmicos pela primeira vez.

---
+++ udemy/projetos/_6_vetores/AverageHeight/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+namespace AverageHeight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número N: ");
+            int num =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura de N pessoas: ");
+
+            float[] heighs = new float[num];
+
+            for (int cont = 0; cont < num; cont++)
+            {
+                heighs[cont] = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            }
+
+            float Avg = Mean(heighs);
+
+            Console.WriteLine($"AVERAGE HEIGHT = {Avg.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+
+        static float Mean(float[] numbers )
+        {
+            float AvgHeight;
+            float sum = 0;
+
+            foreach(float number in numbers)
+            {
+                sum += number;
+            }
+            AvgHeight = sum / numbers.Length;
+
+            return AvgHeight;
+        }        
+    }
+}
+
