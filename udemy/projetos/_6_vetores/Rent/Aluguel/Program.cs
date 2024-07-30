8c700e2fa9d56073b449a3d05f2ba32ab038c55f
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-07-29 23:30:15 -0300

    Trabalhando com um vetor que armazena classes. O vetor trabalhado representava quartos para alugar. Cria-se instancias do nosso objeto para os quartos alugados. temos como propriedades: nome, email e numero do quarto.

---
+++ udemy/projetos/_6_vetores/Rent/Aluguel/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Aluguel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const byte NumRooms = 10;
+            Room[] rooms = new Room[NumRooms];
+
+            Console.WriteLine("Digite a quantidade de estudantes que vão alugar os quartos: ");
+
+            int NumStudents = int.Parse(Console.ReadLine());
+
+            for (int contRoom = 0;  contRoom < NumStudents; contRoom++)
+            {
+                Console.WriteLine($"Rent #{contRoom+1}: ");
+                Console.Write("Name: ");
+                string Name = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string Email = Console.ReadLine();
+
+                Console.Write("Room: ");
+                int roomChoosed = int.Parse(Console.ReadLine());
+
+                rooms[roomChoosed] = new Room(Name, Email, roomChoosed);
+
+                Console.WriteLine();
+            }
+
+            Room.BusyRooms(rooms);
+
+        }
+    }
+}
---
+++ udemy/projetos/_6_vetores/Rent/Aluguel/Room.cs
@@ -0,0 +1,36 @@
+﻿namespace Aluguel
+{
+    internal class Room
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public int NumRoom { get; set; }
+
+        public Room(string name, string email, int numRoom)
+        {
+            Name = name;
+            Email = email;
+            NumRoom = numRoom;
+        }
+
+        public static void BusyRooms(Room[] rooms)
+        {
+            Console.WriteLine("Busy rooms: ");
+            foreach (Room room in rooms)
+            {
+                if (room != null)
+                {
+                    Console.WriteLine(room);
+                }
+
+            }
+        }
+
+        public override string ToString()
+        {
+            string message = $"{NumRoom}: {Name}, {Email}";
+            return message;
+        }
+    }
+}
