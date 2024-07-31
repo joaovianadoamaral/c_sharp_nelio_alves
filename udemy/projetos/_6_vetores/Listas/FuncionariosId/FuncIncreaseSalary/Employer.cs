098554a5b243ea28be511e322932997dddcf38b6
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-07-31 00:33:06 -0300

    Primeiro exercicio de listas. Esta incompleto ainda. Trabalhando com adicionar e pesquisar elementos. Nao foi preciso remover elementos da lista

---
+++ udemy/projetos/_6_vetores/Listas/FuncionariosId/FuncIncreaseSalary/Employer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace FuncIncreaseSalary
+{
+    internal class Employer
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employer(long id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public static double IncreaseSalary(double increasePercentage, double salary)
+        {
+            salary *= (1 + increasePercentage / 100);
+            return salary;
+        }
+
+
+        public static void ChooseIdIncreaseOfList(List<Employer> listEmployer, long idToIncrease, double increasePercentage)
+        {
+            foreach (Employer employer in listEmployer)
+            {
+                if (employer.Id == idToIncrease)
+                {
+                   employer.Salary = IncreaseSalary(increasePercentage, employer.Salary);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string message = $"{Id}, {Name}, {Salary.ToString("F2", CultureInfo.InvariantCulture)}";
+            return message;
+        }
+    }
+}
--- udemy/projetos/_6_vetores/Listas/FuncionariosId/FuncIncreaseSalary/Program.cs
+++ udemy/projetos/_6_vetores/Listas/FuncionariosId/FuncIncreaseSalary/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace FuncIncreaseSalary
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace FuncIncreaseSalary
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("How many employees will be registered? ");
+            int numberEmployees = int.Parse(Console.ReadLine());
+            List<Employer> listEmployee = new List<Employer>();
+
+            Console.WriteLine();
+
+            for (int countEmployees = 0; countEmployees < numberEmployees; countEmployees++)
+            {
+                Console.WriteLine($"Emplyoee #{countEmployees + 1}");
+                Console.Write("Id: ");
+                long Id = long.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string Name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                Console.WriteLine();
+
+                listEmployee.Add(new Employer(Id, Name, Salary));
+
+            }
+
+            Console.Write("Enter the employee id that will have salary increase:  ");
+            long IdWillIncreaseSalary = long.Parse(Console.ReadLine());
+
+
+            Console.Write("Enter the percentage (%):  ");
+            double IncreasePercentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Employer.ChooseIdIncreaseOfList(listEmployee, IdWillIncreaseSalary, IncreasePercentage);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Updated list of employees: ");
+            foreach(  Employer employer in listEmployee)
+            {
+                Console.WriteLine(employer);
+            }
+
         }
     }
 }
