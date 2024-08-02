310ce321f0e5de0fb010f9571063805bad1db583
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2024-08-01 23:20:04 -0300

    Trabalhando com DateTime ( instante ) e TimeSpan ( Duracao). Entendendo o tick, metodos, propriedades e operacoes que facilitam a manipular datas.

---
+++ udemy/projetos/_7_StringsDateTimeTimeSpan/DateTimeAndTimeSpan/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+            Console.WriteLine(dateTime.Ticks);
+
+            TimeSpan timeSpan = new TimeSpan(0, 1, 30);
+            Console.WriteLine(timeSpan);
+
+
+            dateTime = DateTime.Now;
+            int day = dateTime.Day;
+
+            int dayOfYear = dateTime.DayOfYear;
+            Console.WriteLine(dayOfYear);
+
+            DateTime agora = DateTime.Now;
+            DateTime vencimento = agora.AddDays(7);
+
+            Console.WriteLine(agora);
+            Console.WriteLine(vencimento);
+
+        }
+    }
+}
